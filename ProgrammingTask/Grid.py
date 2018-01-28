bf5dc0a1400ef20b2262398d9f360f1ae8c9298b
Richard Michael <richard.michael.1995@gmail.com>
2018-01-28 12:12:08 +0100

    Merge EvaluationGrid into grid - introduce _eval_grid as policy evaluation grid representation, print function for directional symbols

--- ProgrammingTask/Grid.py
+++ ProgrammingTask/Grid.py
@@ -6,7 +6,7 @@ from ProgrammingTask.GridField import GridField
 
 # TODO eliminate those
 # Maybe we can solve this differently? global variables are bad practive in Python
-NOMOVE  = 0
+NOMOVE = 0
 UP = 1
 RIGHT = 2
 DOWN = 3
@@ -25,15 +25,15 @@ DIRECTIONS_D = {NOMOVE: NOMOVE_D, UP: UP_D, RIGHT: RIGHT_D, DOWN: DOWN_D, LEFT: 
 DIRECTION_SYMBOLS = {NOMOVE: "o", UP: '\u25b2', RIGHT: '>', DOWN: 'V', LEFT: '<'}
 
 
-
 class Grid:
     '''
     Grid class, initializes grid from file, reads input file
-    and instantiates fields accordingly
+    and instantiates fields accordingly.
+    Writes grid-values to _grid.
     has get and set methods for field and string representation
     '''
 
-    def __init__(self, grid_file, separator=" ", policy_grid=None):
+    def __init__(self, grid_file, separator=" ", policy_grid=None, initial_policy_eval=None):
         '''
         Constructor of Grid takes input file and reads it into array
         that is used to instantiate the Fields from GridFields
@@ -48,7 +48,8 @@ class Grid:
                 self._array.append(weight_list)
         # use grid values as input for field factory and instantiate the grid
         self._grid = [[GridField.factory(field_type) for field_type in line] for line in self._array]
-        self._policy_grid = self.set_policy(policy_grid) if policy_grid else self.set_random_policy()
+        self._policy_grid = policy_grid if policy_grid else self.set_random_policy()
+        self._eval_grid = initial_policy_eval if initial_policy_eval else self.set_policy_evaluation_zero()
 
     def __str__(self):
         out = ""
@@ -73,53 +74,44 @@ class Grid:
         y = len(self._grid[0])
         return [x, y]
 
-    def set_policy(self, policy_grid):
-        self._grid = policy_grid
-
     def set_random_policy(self):
-       # generate random initialisation of Direction array - exclude NOMOVE by starting at 1
-       random_directions = [[DIRECTIONS[randint(1, len(DIRECTIONS)-1)] for y in range(self.shape[1])]
-                                 for x in range(self.shape[0])]
-       self.set_policy(random_directions)
-
-
-class PolicyEvaluationGrid(Grid):
-    """
-    Contains a policy evaluation.
-    2dimensional array with evaluation values
-    """
-
-    def __init__(self, shape, initial_policy_evaluation=None):
-        super().__init__()
-        if initial_policy_evaluation is None:
-            self.set_policy_evaluation_zero(shape)
-        else:
-            self.set_policy_evaluation(initial_policy_evaluation)
-
-    def set_policy_evaluation(self, policy_evaluation_grid):
-        self._grid = policy_evaluation_grid
+        '''
+       Generate random initialisation of Direction for policy grid
+       :return array:
+       '''
+        # exclude NOMOVE by starting at 1
+        random_directions = [[DIRECTIONS[randint(1, len(DIRECTIONS) - 1)] for y in range(self.shape[1])]
+                             for x in range(self.shape[0])]
+        return random_directions
 
     def set_policy_evaluation_zero(self, shape):
-        self._grid = np.zeros(shape)
+        '''
+        initializes array of zero as initial evaluation grid
+        :param shape:
+        '''
+        self._eval_grid = [[0 * x for x in range(shape(1))] for y in range(shape[0])]
 
-
-    def __str__(self):
+    def print(self):
+        '''
+        Converts Grid values into direction symbols and print them
+        '''
         field_string = ""
         line_old = 0
         for line, row in itertools.product(range(self.shape[0]), range(self.shape[1])):
             if not line_old == line:
                 field_string += "\n"
                 line_old = line
-            field_string += DIRECTION_SYMBOLS[self.get_field(line, row)]
+                # uses policy values (directions) as key-value for direction symbols
+            field_string += DIRECTION_SYMBOLS[self._policy_grid[line][row]]
             field_string += " "
-        return field_string
+        print(field_string)
 
 
 # instantiate parser
 parser = argparse.ArgumentParser(description='''Read grid-file from stdin,
 parse and print grid file accordingly.''')
 parser.add_argument('grid_file', help='path to input .grid file')
-#parser.add_argument('-i', '--iter', type='int',
+# parser.add_argument('-i', '--iter', type='int',
 #                   help='how many iterations are performed by the policy iteration', )
 args = parser.parse_args()
 
@@ -128,9 +120,9 @@ def main():
     # those would be test cases - ToDo put into pytest under ./test when done
     grid = Grid(grid_file=args.grid_file)
     print(grid)
-    print('Get field at [0,0]... {}'.format(grid.get_field(0,0)))
-    print('Get field at [1,1]... {}'.format(grid.get_field(1,1)))
-    grid.set_field(1,1,"P")
+    print('Get field at [0,0]... {}'.format(grid.get_field(0, 0)))
+    print('Get field at [1,1]... {}'.format(grid.get_field(1, 1)))
+    grid.set_field(1, 1, "P")
     print('Adding Penalty at [1,1]...')
     print(grid)
     print('Get Shape Property of grid...')
