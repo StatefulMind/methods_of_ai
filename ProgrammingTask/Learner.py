daa20a96f986f24fff1773127861f78351fc7fec
Richard Michael <richard.michael.1995@gmail.com>
2018-02-28 19:53:15 +0100

    updated learn function introduced next and previous values and arrays

--- ProgrammingTask/Learner.py
+++ ProgrammingTask/Learner.py
@@ -8,12 +8,12 @@ class Learner:
     Learner generated policy and improves on it
     '''
 
-    def __init__(self, grid, position='static', learning_rate=0.04, reward_decay=0.04,
+    def __init__(self, grid, position='static', learning_rate=0.04, reward_decay=0.4,
                  epsilon_soft=0.4):
         self._grid = grid
         self._pos = self.start_position(position)
         self._learning_rate = learning_rate
-        self._reward_decay = reward_decay
+        self._gamma = reward_decay
         self._epsilon_soft = epsilon_soft
 
     def start_position(self, position):
@@ -34,13 +34,56 @@ class Learner:
     def random_start(self):
         return np.random.randint(self._grid.shape_x), np.random.randint(self._grid.shape_y)
 
-    def evaluate_policy(self):
-        field_field = self._grid.get_grid_field(self._pos[0], self._pos[1])
-        policy_of_field = self._grid.get_policy_field(self._pos[0], self._pos[1])
-        print(field_field)
-        print(policy_of_field)
-        new_array = [[0 for _ in range(self._grid.shape_x)] for _ in range(self._grid.shape_y)]
+    def learn(self, iterations,  convergence = None):
+        for _ in range(iterations)
+            x = self._pos[0]
+            y = self._pos[1]
+            field = self._grid.get_grid_field(x, y)
+            policy_of_field = self._grid.get_policy_field(x, y)
+            print(field)
+            print(policy_of_field)
+            # array in the same shape for adding values later
+            new_array = [[0 for _ in range(self._grid.shape_x)] for _ in range(self._grid.shape_y)]
+
+            # do (all possible) policy actions while checking if state exists
+            movement = field.get_movement_probs()[policy_of_field]
+
+            next_reward = 0
+            for direction, probability in movement.items():
+                # go into direction
+                x_next, y_next = np.add([x, y], DIRECTIONS_D[direction])
+                possible_next_field = self._grid.get_grid_field(x_next, y_next)
+                # check if out of bounds
+                if is_out_of_bounds(x_next, y_next, self._grid.shape):
+                    x_next, y_next = [x, y]
+                elif not possible_next_field.can_move_here:
+                    x_next, y_next = [x, y]
+                next_reward += probability * prev_array[x_next][y_next]# next array state or previous array state
+                if possible_next_field.type == 'P' or possible_next_field.type == 'E':
+                    # return value when next field terminal
+                    new_array[x_next][y_next] = possible_next_field.get_static_evaluation_value
+                else:
+                    new_array[x_next][y_next] -= self.learning_rate * self._gamma
+                    ### ToDo
+
+
+
+
+            # check for convergence - difference of value arrays
+            if not convergence is None and self.check_convergence(prev_array, new_array,
+                                                                  convergence_value=convergence):
+                print('convergence value reached...')
+                break
+
+            prev_array = new_array
+        self._grid.set_eval_grid(new_array)
+
+
         print(field_field.get_movement_probs()[policy_of_field])
+
+    # action corresponds to MOVEMENT > FROM MOVEMENT_D by policy
+    def apply_epsilon(self, action):
+        return np.random.uniform(#SOMETHING) # choose with epsilon soft policy from possible movements
 
     def improve_policy(self):
         pass
